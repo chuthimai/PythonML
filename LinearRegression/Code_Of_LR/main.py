57916abe0d4f5e6190b348db92787b845408b50e
Chu Mai <chuthimai18245@gmail.com>
2023-01-12 23:29:54 +0700

    Demo LR
    using GD momentum but can't run in the loop

--- LinearRegression/Code_Of_LR/main.py
+++ LinearRegression/Code_Of_LR/main.py
@@ -1,13 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-#numOfPoint = 30
-#noise = np.random.normal(0,1,numOfPoint).reshape(-1,1)
-#x = np.linspace(30, 100, numOfPoint).reshape(-1,1)
-#N = x.shape[0]
-#y = 15*x + 8 + 20*noise
-#plt.scatter(x, y)
-plt.subplot2grid((5, 2), (0, 0), rowspan=2)
+
+# plt.subplot2grid((5, 2), (0, 0), rowspan=2)
 data = pd.read_csv('data.csv').values
 N = data.shape[0]
 x = data[:, 0].reshape(-1, 1)
@@ -17,24 +12,41 @@ plt.xlabel('mét vuông')
 plt.ylabel('giá')
 
 x = np.hstack((np.ones((N, 1)), x, np.multiply(x, x)))
-w = np.array([0., 1., 1.]).reshape(-1, 1)
+w = np.array([190, -97, 1.]).reshape(-1, 1)
 
-numOfIteration = 10
-cost = np.zeros((numOfIteration, 1))
-learning_rate = 0.001
-for i in range(1, numOfIteration):
+# # Use Linear Regession
+# x_T = np.transpose(x)
+# invert_x = np.linalg.inv(np.dot(x_T, x))
+# w = np.dot(np.dot(invert_x, x_T), y)
+
+# Use Gradient descent
+learning_rate = 0.01
+r = np.dot(x, w) - y
+v = np.zeros_like(w).reshape(-1, 1)
+gamma = 0.9
+while abs(np.average(r)) > 100:
+    v[0] = gamma * v[0] + learning_rate * np.average(r * x[:, 0].reshape(-1, 1))
+    w[0] = w[0] - v[0]
+    v[1] = gamma * v[1] + learning_rate * np.average(r * x[:, 1].reshape(-1, 1))
+    w[1] = w[1] - v[1]
+    v[2] = gamma * v[2] + learning_rate * np.average(r * x[:, 2].reshape(-1, 1))
+    w[2] = w[2] - v[2]
+
     r = np.dot(x, w) - y
-    cost[i] = 0.5*np.sum(r*r) # cost is loss function
-    w[0] -= learning_rate*np.sum(r)
-    # correct the shape dimension
-    w[1] -= learning_rate*np.sum(np.multiply(r, x[:, 1].reshape(-1, 1)))
-    w[2] -= learning_rate*np.sum(np.multiply(r, x[:, 2].reshape(-1, 1)))
-    print(cost[i])
+
 predict = np.dot(x, w)
-plt.subplot2grid((5, 2), (3, 0), rowspan=2)
-plt.plot(x[:, 1], predict, 'r')
+# plt.subplot2grid((5, 2), (3, 0), rowspan=2)
+plt.plot(x[:, 1], predict, c="#E98EAD")
+plt.xlabel('mét vuông')
+plt.ylabel('giá')
 plt.show()
 
 x1 = 50
 y1 = w[0] + w[1] * x1 + w[2] * x1 * x1
 print('Giá nhà cho 50m^2 là : ', y1)
+print(w)
+print(abs(np.average(r)))
+
+
+
+
